4cbb0a98e558e9b15d5ab4695eed7c9d92459674
Sergey Shishkin <sergey@shishkin.org>
2022-08-16 20:18:19 +0200

    convert site to astro

---
+++ astro.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import mdx from "@astrojs/mdx";
+
+export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  integrations: [mdx()],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+});
