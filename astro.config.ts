786e376146864a0b76d487df7440a97b8bfe0907
Sergey Shishkin <sergey@shishkin.org>
2024-01-07 13:38:28 +0100

    fix astro-icon

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,12 @@ import type { AstroIntegration } from "astro";
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [mdx() as AstroIntegration],
+  integrations: [icon(), mdx() as AstroIntegration],
   vite: {
     ssr: {
       external: ["svgo"],
