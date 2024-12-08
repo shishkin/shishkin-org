f3875db0b4412778a78c7d00e8175f77f3736ccc
Sergey Shishkin <sergey@shishkin.org>
2024-12-08 10:04:35 +0100

    chore: update astro and remove vercel

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,10 @@
 import type { AstroIntegration } from "astro";
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
+  output: "static",
   integrations: [icon(), mdx() as AstroIntegration],
   vite: {
     ssr: {
