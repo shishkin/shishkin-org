aa952ed25ca6fd473c0c700b87b29949769a5d02
Sergey Shishkin <sergey@shishkin.org>
2026-05-09 12:44:51 +0200

    refactor: remove astro-icon

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import icon from "astro-icon";
 
 export default defineConfig({
   output: "static",
-  integrations: [icon(), mdx()],
+  integrations: [mdx()],
   devToolbar: { enabled: false },
   vite: {
     ssr: {
